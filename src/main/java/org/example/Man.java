ec229c5be6cae0a8740df4899c580d5d62060322
khalidhussain2021 <hussain2020mdkhalid@gmail.com>
2024-08-15 16:06:19 +0530

    Set class

--- src/main/java/org/example/Man.java
+++ src/main/java/org/example/Man.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Man {
     public static void main(String[] args) {
-        System.out.println("Khalid1");
-    }
+        Set<String> fruits=new HashSet<>();
+        fruits.add(new String("banana"));
+     }
 }
