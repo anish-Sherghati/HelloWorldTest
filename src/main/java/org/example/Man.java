9cc9c32f0cff62072a3e290742b976c68ede59e7
khalidhussain2021 <hussain2020mdkhalid@gmail.com>
2024-08-15 16:19:51 +0530

    set value added

--- src/main/java/org/example/Man.java
+++ src/main/java/org/example/Man.java
@@ -2,10 +2,17 @@ package org.example;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Man {
     public static void main(String[] args) {
-        Set<String> fruits=new HashSet<>();
+        Set<String> fruits=new HashSet<String>();
         fruits.add(new String("banana"));
-     }
+        fruits.add(new String("papaya"));
+        fruits.add(new String("mango"));
+        fruits.add(new String("grapsh"));
+        Set<String> sortedFruites = fruits.stream().sorted().collect(Collectors.toSet());
+        System.out.println(sortedFruites);
+
+    }
 }
