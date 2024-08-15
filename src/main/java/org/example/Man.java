6260111cc749a73e8748e7e79638da82764c4038
khalidhussain2021 <hussain2020mdkhalid@gmail.com>
2024-08-15 18:06:09 +0530

    khalid3

--- src/main/java/org/example/Man.java
+++ src/main/java/org/example/Man.java
@@ -3,5 +3,6 @@ package org.example;
 public class Man {
     public static void main(String[] args) {
         System.out.println("Khalid1");
+        System.out.println("khalid hussain");
     }
 }
